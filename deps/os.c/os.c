2a87257a9959bdf75dd6d439a177b06e54b08bde
Abraham Hernandez <abraham@abranhe.com>
2019-04-25 01:33:15 -0400

    add deps folder to the repository

---
+++ deps/os.c/os.c
@@ -0,0 +1,31 @@
+//
+// os.c
+// Get to know your operating system
+//
+// MIT licensed.
+// Copyright (c) Abraham Hernandez <abraham@abranhe.com>
+//
+
+/**
+ * Return Operating System
+ */
+
+const char 
+*operating_system(void)
+{
+    #ifdef _WIN32
+    return "win32";
+    #elif _WIN64
+    return "win64";
+    #elif __unix || __unix__
+    return "unix";
+    #elif __APPLE__ || __MACH__
+    return "macOS";
+    #elif __linux__
+    return "linux";
+    #elif __FreeBSD__
+    return "freeBSD";
+    #else
+    return "other";
+    #endif
+}
